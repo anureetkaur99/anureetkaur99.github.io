9864c6f844f53e7790dbfab5d476907c883baad9
anureetkaur99 <kauranureet99@gmail.com>
2021-03-08 21:26:28 -0800

    Fixing the todo issue

--- js/week5.js
+++ js/week5.js
@@ -1,27 +1,23 @@
 window.onload = function() {
     const todoList = document.querySelector('ul');
     const input = document.querySelector('input');
-    const button = document.querySelector('button');
-    
-    button.onClick = function() {
-        let myTask = input.value;
-        input.value = '';
-
-        const listTodo = document.createElement('li');
-        const listText = document.createElement('span');
-        const listButton = document.createElement('button');
-    
-        listTodo.appendChild(listText);
-        listText.textContent = myTask;
-        listTodo.appendChild(listButton);
-        listButton.textContent = 'Delete';
-        listButton.style.marginLeft = '10px';
-        todoList.appendChild(listTodo);
-    
-        listButton.onclick = function(e) {
-            todoList.removeChild(listTodo);
-        }
-    
-        input.focus();
+    const addButton = document.querySelector('button');
+  
+    addButton.onclick = function() {
+      const listTodo = document.createElement('li');
+      const listText = document.createElement('span');
+      const listButton = document.createElement('button');
+  
+      listTodo.appendChild(listText);
+      listTodo.classList.add('magic-list');
+      listText.textContent = input.value;
+      listButton.textContent = 'Delete';
+      listButton.classList.add('delete-button');
+      listTodo.appendChild(listButton);
+      todoList.appendChild(listTodo);
+  
+      listButton.onclick = function(e) {
+        todoList.removeChild(listTodo);
+      }
     }
-}
+  }
